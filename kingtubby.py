ed33a9d4848eeb6fb0f0aeaa2f7bd3e7a826d2a5
brvnv <bruno.rdx@metalfan.com>
2021-06-21 15:13:55 -0300

    Mais um dub adicionado

--- kingtubby.py
+++ kingtubby.py
@@ -5,11 +5,16 @@ def vocalDub (dubplate):
 def skankDub (dubplate):
     print(dubplate+dubplate[len(dubplate)-4:]*25)
 
+def solomonDub(dubplate):
+    print(dubplate+dubplate[len(dubplate)-2:]*25)
+
 while True:
     userString=input("Digite uma frase ou /q para sair")
     if userString=="/q":
         sys.exit()
     elif len(userString)==5:
         skankDub(userString)
+    elif len(userString)==3:
+        solomonDub(userString)
     else:
         vocalDub (userString)
